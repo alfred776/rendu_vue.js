a77f3d5e983839c7540f49bae8b59c78b62be735
alfred776 <117074788+alfred776@users.noreply.github.com>
2023-01-18 18:46:12 +0100

    rendu vue

---
+++ src/views/journalView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="personnage">
+        <div class="journal">
+            <p>0102030405</p>
+            <p>ANNE</p>
+        </div>
+
+       <div>
+        <p>0650809127</p>
+        <p>Allan</p>
+       </div>
+
+       <div>
+          <p>0758084135</p>
+          <P>jovie</P>
+       </div>
+
+    </div>
+</template>
